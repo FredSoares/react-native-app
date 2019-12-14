f0c1fad2bc175ec350aa3ee629e4519d15b54221
Frederico Soares <soaresfred89@gmail.com>
2019-12-14 00:05:04 -0100

    Styling form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Form, Input, SubmitButton} from './styles';
 
 class Main extends React.Component {
   render() {
-    return <Container />;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuario"
+          />
+          <SubmitButton>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
   }
 }
 
