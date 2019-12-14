c5255c402c28b2b22418bc4d472ddc9398767d16
Frederico Soares <soaresfred89@gmail.com>
2019-12-14 00:35:14 -0100

    Access Github API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
-
+import {Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import {Container, Form, Input, SubmitButton} from './styles';
 
 class Main extends React.Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const {users, newUser} = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
   render() {
+    const {users, newUser} = this.state;
+
     return (
       <Container>
         <Form>
@@ -13,8 +41,11 @@ class Main extends React.Component {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Adicionar usuario"
+            onChangeText={text => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton>
+          <SubmitButton onPress={this.handleAddUser}>
             <Icon name="add" size={20} color="#fff" />
           </SubmitButton>
         </Form>
